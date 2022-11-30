2443aba8971c4498f978c2f7cc3f7ce2e3e41046
TimurMP <titimurmamyshev@gmail.commur>
2022-11-30 03:12:01 +0200

    findBooksByPublisher added

--- src/main/java/telran/java2022/book/dao/BookRepository.java
+++ src/main/java/telran/java2022/book/dao/BookRepository.java
@@ -3,6 +3,7 @@ package telran.java2022.book.dao;
 import org.springframework.data.repository.CrudRepository;
 import telran.java2022.book.model.Author;
 import telran.java2022.book.model.Book;
+import telran.java2022.book.model.Publisher;
 
 import java.util.Set;
 import java.util.stream.Stream;
@@ -11,5 +12,7 @@ public interface BookRepository extends CrudRepository<Book, String> {
 
 //    @Query("select b from Book b join Author a where a.name = ?1")
     Stream<Book> findAllByAuthorsIsIn(Set<Author> authors);
+
+    Stream<Book> findBooksByPublisher(Publisher publisher);
 
 }
--- src/main/java/telran/java2022/book/service/BookServiceImpl.java
+++ src/main/java/telran/java2022/book/service/BookServiceImpl.java
@@ -82,9 +82,12 @@ public class BookServiceImpl implements BookService {
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public Iterable<BookDto> findBooksByPublisher(String publisherName) {
-		// TODO Auto-generated method stub
-		return null;
+		Publisher publisher = publisherRepository.findById(publisherName).orElseThrow(EntityNotFoundException::new);
+		return bookRepository.findBooksByPublisher(publisher)
+				.map(book -> modelMapper.map(book, BookDto.class))
+				.collect(Collectors.toList());
 	}
 
 	@Override
